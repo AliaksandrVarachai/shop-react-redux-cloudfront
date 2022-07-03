800b4e625e7308cb5ac06bfbb874b88f499bb6e1
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2022-07-04 00:32:14 +0300

    Integrate products import with FE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://6t8amjybxg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rf0c5q2bb7.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
