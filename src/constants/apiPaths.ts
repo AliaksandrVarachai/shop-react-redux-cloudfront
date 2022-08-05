a712e1290224c81f27550d201b7e40389e889778
Aliaksandr Varachai <aliaksandr_varachai@epam.com>
2022-08-05 23:40:16 +0300

    Integrate a cart (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,8 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://6t8amjybxg.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://rf0c5q2bb7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://varachai-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
+  // cart: 'http://localhost:4000/api',
 };
 
 export default API_PATHS;
