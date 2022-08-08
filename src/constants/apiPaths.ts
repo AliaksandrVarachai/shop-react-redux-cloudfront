4dc5d290f040f4a6011aa45a38e678c61f1c75c1
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2022-08-08 14:04:13 +0300

    Update Product and Cart URLs

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,8 +4,6 @@ const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://6t8amjybxg.execute-api.eu-west-1.amazonaws.com/dev',
-  // bff: 'https://rf0c5q2bb7.execute-api.eu-west-1.amazonaws.com/dev',
-  // cart: 'https://278w6q7cf7.execute-api.eu-west-1.amazonaws.com/api',
   bff: bffApiGatewayUrl,
   cart: bffApiGatewayUrl,
 };
