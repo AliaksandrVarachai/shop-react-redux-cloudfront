d0d67617aee4f1d37f16b579b4c1489cf2a4258f
Aliaksandr Varachai <Aliaksandr_Varachai@epam.com>
2022-08-08 12:52:58 +0300

    Use BFF for Product and Cart services

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,11 +1,13 @@
+const bffApiGatewayUrl = 'https://tou4gq4voh.execute-api.eu-west-1.amazonaws.com'
 
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://6t8amjybxg.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://rf0c5q2bb7.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://278w6q7cf7.execute-api.eu-west-1.amazonaws.com/api',
-  // cart: 'http://localhost:4000/api',
+  // bff: 'https://rf0c5q2bb7.execute-api.eu-west-1.amazonaws.com/dev',
+  // cart: 'https://278w6q7cf7.execute-api.eu-west-1.amazonaws.com/api',
+  bff: bffApiGatewayUrl,
+  cart: bffApiGatewayUrl,
 };
 
 export default API_PATHS;
